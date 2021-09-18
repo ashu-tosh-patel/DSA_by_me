48da5c86551de5531db813788853df18e2c9030c
Ashu <ashutoshpatelnoida@gmail.com>
2021-09-18 22:36:10 +0530

    Implemented bitwise algos

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+        BitWise bit= new BitWise();
+        System.out.println(bit.divideBy2(8));
+        System.out.println(bit.multiplyBy2(8));
+        System.out.println(bit.log2(8));
+
+    }
+}
